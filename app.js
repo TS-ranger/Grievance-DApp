64bdd7a4fb1689e11ed13deefca733de41b09807
Tushar Saha <sahatushar242002@gmail..com>
2026-04-07 21:27:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,265 @@
+let web3;
+let contract;
+let account;
+
+const contractAddress = "0x4071227F181B712ac9A2608993791ea784e7295C";
+const abi = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "student",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "category",
+        "type": "string"
+      }
+    ],
+    "name": "GrievanceCreated",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "status",
+        "type": "string"
+      }
+    ],
+    "name": "StatusUpdated",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_description",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_category",
+        "type": "string"
+      }
+    ],
+    "name": "submitGrievance",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_status",
+        "type": "string"
+      }
+    ],
+    "name": "updateStatus",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "admin",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      }
+    ],
+    "name": "getGrievance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "grievanceCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "grievances",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "student",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "description",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "category",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "status",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "timestamp",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+window.onload = async () => {
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+
+        const accounts = await web3.eth.getAccounts();
+        account = accounts[0];
+
+        contract = new web3.eth.Contract(abi, contractAddress);
+
+        loadGrievances();
+    }
+};
+
+async function submitGrievance() {
+    const description = document.getElementById("description").value;
+    const category = document.getElementById("category").value;
+
+    await contract.methods
+        .submitGrievance(description, category)
+        .send({ from: account });
+
+    alert("Submitted!");
+    loadGrievances();
+}
+
+async function loadGrievances() {
+    const count = await contract.methods.grievanceCount().call();
+
+    const container = document.getElementById("grievanceList");
+    container.innerHTML = "";
+
+    for (let i = 1; i <= count; i++) {
+        const g = await contract.methods.getGrievance(i).call();
+
+        container.innerHTML += `
+            <div class="card">
+                <p><b>ID:</b> ${g[0]}</p>
+                <p><b>Description:</b> ${g[2]}</p>
+                <p><b>Category:</b> ${g[3]}</p>
+                <p class="status ${g[4].replace(" ", "")}">
+                    ${g[4]}
+                </p>
+
+                <button onclick="updateStatus(${g[0]}, 'InReview')">In Review</button>
+                <button onclick="updateStatus(${g[0]}, 'Resolved')">Resolve</button>
+            </div>
+        `;
+    }
+}
+
+async function updateStatus(id, status) {
+    await contract.methods
+        .updateStatus(id, status)
+        .send({ from: account });
+
+    loadGrievances();
+}
